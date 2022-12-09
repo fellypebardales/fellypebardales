1220f970e203234fcd3dcbed773945a36cd3e392
fellypebardales <86852698+fellypebardales@users.noreply.github.com>
2022-12-09 03:39:44 -0300

    f

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,61 +1,67 @@
-var img = document.querySelector("#img-item");
+// var img = document.querySelector("#img-item");
 
-var item = document.querySelector("#item1");
-item.addEventListener ("mouseover", function() {
-    img.setAttribute('src', 'assets/images/img1.png');
-})
-item.addEventListener ("mouseout", function() {
-    img.setAttribute('src', 'assets/images/cesto-inicial.png');})
+// var item = document.querySelector("#item1");
+// item.addEventListener ("mouseover", function() {
+//     img.setAttribute('src', 'assets/images/img1.png');
+// })
+// item.addEventListener ("mouseout", function() {
+//     img.setAttribute('src', 'assets/images/cesto-inicial.png');})
 
 
-var item = document.querySelector("#item2");
-item.addEventListener ("mouseover", function() {
-    img.setAttribute('src', 'assets/images/img2.png');})
-item.addEventListener ("mouseout", function() {
-    img.setAttribute('src', 'assets/images/cesto-inicial.png');})
+// var item = document.querySelector("#item2");
+// item.addEventListener ("mouseover", function() {
+//     img.setAttribute('src', 'assets/images/img2.png');})
+// item.addEventListener ("mouseout", function() {
+//     img.setAttribute('src', 'assets/images/cesto-inicial.png');})
 
     
-    var item = document.querySelector("#item3");
-item.addEventListener ("mouseover", function() {
-    img.setAttribute('src', 'assets/images/img3.png');})
-item.addEventListener ("mouseout", function() {
-    img.setAttribute('src', 'assets/images/cesto-inicial.png');})
+//     var item = document.querySelector("#item3");
+// item.addEventListener ("mouseover", function() {
+//     img.setAttribute('src', 'assets/images/img3.png');})
+// item.addEventListener ("mouseout", function() {
+//     img.setAttribute('src', 'assets/images/cesto-inicial.png');})
 
 
-    var item = document.querySelector("#item4");
-item.addEventListener ("mouseover", function() {
-    img.setAttribute('src', 'assets/images/img4.png');})
-item.addEventListener ("mouseout", function() {
-    img.setAttribute('src', 'assets/images/cesto-inicial.png');})
+//     var item = document.querySelector("#item4");
+// item.addEventListener ("mouseover", function() {
+//     img.setAttribute('src', 'assets/images/img4.png');})
+// item.addEventListener ("mouseout", function() {
+//     img.setAttribute('src', 'assets/images/cesto-inicial.png');})
 
 
-    var item = document.querySelector("#item5");
-item.addEventListener ("mouseover", function() {
-    img.setAttribute('src', 'assets/images/img5.png');})
-item.addEventListener ("mouseout", function() {
-    img.setAttribute('src', 'assets/images/cesto-inicial.png');})
+//     var item = document.querySelector("#item5");
+// item.addEventListener ("mouseover", function() {
+//     img.setAttribute('src', 'assets/images/img5.png');})
+// item.addEventListener ("mouseout", function() {
+//     img.setAttribute('src', 'assets/images/cesto-inicial.png');})
 
 
-    var item = document.querySelector("#item6");
-item.addEventListener ("mouseover", function() {
-    img.setAttribute('src', 'assets/images/img6.png');})
-item.addEventListener ("mouseout", function() {
-    img.setAttribute('src', 'assets/images/cesto-inicial.png');})
+//     var item = document.querySelector("#item6");
+// item.addEventListener ("mouseover", function() {
+//     img.setAttribute('src', 'assets/images/img6.png');})
+// item.addEventListener ("mouseout", function() {
+//     img.setAttribute('src', 'assets/images/cesto-inicial.png');})
 
 
-    var item = document.querySelector("#item7");
-item.addEventListener ("mouseover", function() {
-    img.setAttribute('src', 'assets/images/img7.png');})
-item.addEventListener ("mouseout", function() {
-    img.setAttribute('src', 'assets/images/cesto-inicial.png');})
+//     var item = document.querySelector("#item7");
+// item.addEventListener ("mouseover", function() {
+//     img.setAttribute('src', 'assets/images/img7.png');})
+// item.addEventListener ("mouseout", function() {
+//     img.setAttribute('src', 'assets/images/cesto-inicial.png');})
 
 
-    var item = document.querySelector("#item8");
-item.addEventListener ("mouseover", function() {
-    var mouse = document.getElementById('objeto');
-    img.setAttribute('src', 'assets/images/img8.png');})
-item.addEventListener ("mouseout", function() {
-    img.setAttribute('src', 'assets/images/cesto-inicial.png');})
+//     var item = document.querySelector("#item8");
+// item.addEventListener ("mouseover", function() {
+//     var mouse = document.getElementById('objeto');
+//     img.setAttribute('src', 'assets/images/img8.png');})
+// item.addEventListener ("mouseout", function() {
+//     img.setAttribute('src', 'assets/images/cesto-inicial.png');})
 
 
-      $("h5").fadeOut("5000").fadeIn("2000").fadeOut("2000");
+    $("#it").mouseenter(function () {
+        $("#item1").first().fadeOut("fast", "linear");
+      });
+
+      $("button").mouseout(function () {
+        $("#item1").first().fadeIn("fast", "linear");
+      });
